027259a45a69e4d2d2d32a6b2ef0e4b1da6ae87a
CGIRALDO <cgiraldo@bancolombia.com.co>
2021-06-09 23:08:04 -0500

    modeule 4

--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,5 @@
+import sys
+
 from pathlib import Path
 
 class Site:
@@ -23,7 +25,7 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not implemented")
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
 
 
     def build(self):
@@ -33,3 +35,8 @@ class Site:
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+
+    
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
